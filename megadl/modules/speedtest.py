b9116d0a926d11aff432481fdba654b64d8e9fde
Rajesh Sharma <115607172+RajeshSharma46@users.noreply.github.com>
2022-10-24 07:15:13 +0530

    Update speedtest.py

--- megadl/modules/speedtest.py
+++ megadl/modules/speedtest.py
@@ -37,7 +37,7 @@ async def speedtest_function(client, message):
 **__Sponsor:__** {result['server']['sponsor']}
 **__Latency:__** {result['server']['latency']}  
 **__Ping:__** {result['ping']}"""
-    msg = await app.send_photo(
+    msg = await Client.send_photo(
         chat_id=message.chat.id, 
         photo=result["share"], 
         caption=output
